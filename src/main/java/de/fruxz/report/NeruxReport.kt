a1839896c1a17e43e41490bcf3f1f84c44b198cc
Fruxz <cedricspitzer@outlook.de>
2020-09-22 16:45:36 +0200

    Transmissions now Configuration serializable

--- src/main/java/de/fruxz/report/NeruxReport.kt
+++ src/main/java/de/fruxz/report/NeruxReport.kt
@@ -1,6 +1,7 @@
 package de.fruxz.report
 
 import de.fruxz.report.domain.Report
+import de.fruxz.report.domain.Transmission
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 import org.bukkit.configuration.serialization.ConfigurationSerialization
 import org.bukkit.plugin.java.JavaPlugin
@@ -9,6 +10,7 @@ class NeruxReport : JavaPlugin() {
 
     override fun onLoad() {
         ser(Report::class.java)
+        ser(Transmission::class.java)
         super.onLoad()
     }
 
