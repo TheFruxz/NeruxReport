b41233f8f4d6078e54f0eb904b974b1c9eeb8847
Fruxz <cedricspitzer@outlook.de>
2020-09-22 16:31:55 +0200

    Report class object is now configuration serializable

--- src/main/java/de/fruxz/report/NeruxReport.kt
+++ src/main/java/de/fruxz/report/NeruxReport.kt
@@ -1,10 +1,14 @@
 package de.fruxz.report
 
+import de.fruxz.report.domain.Report
+import org.bukkit.configuration.serialization.ConfigurationSerializable
+import org.bukkit.configuration.serialization.ConfigurationSerialization
 import org.bukkit.plugin.java.JavaPlugin
 
 class NeruxReport : JavaPlugin() {
 
     override fun onLoad() {
+        ser(Report::class.java)
         super.onLoad()
     }
 
@@ -14,6 +18,10 @@ class NeruxReport : JavaPlugin() {
 
     override fun onDisable() {
         super.onDisable()
+    }
+
+    private fun ser(clazz: Class<out ConfigurationSerializable>) {
+        ConfigurationSerialization.registerClass(clazz)
     }
 
 }
