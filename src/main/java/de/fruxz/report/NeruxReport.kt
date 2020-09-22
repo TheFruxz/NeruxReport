3900abe52436a33bdd9131fbda297b259540cc50
Fruxz <cedricspitzer@outlook.de>
2020-09-22 16:07:41 +0200

    Basic structure initialization

---
+++ src/main/java/de/fruxz/report/NeruxReport.kt
@@ -0,0 +1,19 @@
+package de.fruxz.report
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NeruxReport : JavaPlugin() {
+
+    override fun onLoad() {
+        super.onLoad()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+}
